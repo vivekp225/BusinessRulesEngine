c11025e003bb32c8c1a88371d422dce337f70938
Vivek Pandey <HenGapNoi9Suoi@hotmail.com>
2021-05-01 15:48:07 +0100

    Adding Test Project

---
+++ BusinessRulesEngineTest/ProgramTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace BusinessRulesEngineTest
+{
+    public class ProgramTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
